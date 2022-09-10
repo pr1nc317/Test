a800542f61f8d2ad699f8f3d2326080a2b82e138
Amit Sharma <amitsharma171094@outlook.com>
2022-09-10 19:59:45 +0530

    9 Lakh Numbers Utility + Hector Help

---
+++ 9LakhNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _9LakhNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "";
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    str += "+919999999999,";
+            //}
+            //str = str.Substring(0, str.Length - 1);
+            //File.AppendAllText("C:\\Users\\Amit Sharma\\Source\\Repos\\pr1nc317\\Test\\Test\\abc.txt", str);
+
+            List<string> list = new List<string>();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 100000; i++)
+            {
+                list.Add("+919999999999");
+                
+            }
+            stringBuilder = stringBuilder.AppendJoin(',', list);
+            File.AppendAllText("C:\\Users\\Amit Sharma\\Source\\Repos\\pr1nc317\\Test\\Test\\abc.txt", str);
+        }
+    }
+}
---
+++ HectorHelp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HectorHelp
+{
+    using System;
+    using System.IO;
+    using System.Xml;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            string contents = null;
+            using (StreamReader sr = new StreamReader("C:\\Users\\Amit Sharma\\Downloads\\803178010040688.xml", System.Text.Encoding.UTF8))
+            {
+                contents = sr.ReadToEnd();
+            }
+            doc.LoadXml(contents);
+
+
+            ///// EXTRA /////
+            var xmlList = doc.GetElementsByTagName("party");
+            for (int i = 0; i < xmlList.Count; i++)
+            {
+                Console.WriteLine(xmlList[i].ChildNodes.Item(i).InnerText);
+            }
+
+            //foreach (var item in xmlList.)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine(doc.GetElementsByTagName("party")[1].InnerXml);
+
+        }
+    }
+}
+
